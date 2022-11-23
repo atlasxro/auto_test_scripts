286b301c10ffb3b192678dd473b49a206e6a9fa5
atlas.luo <atlas.luo@starfivetech.com>
2022-11-23 13:46:09 +0800

    Fix some bugs

--- log_gen.py
+++ log_gen.py
@@ -10,7 +10,7 @@ def mkdir(path): #自定义日志文件夹创建函数
         os.makedirs(path)  #makedirs 创建文件时如果路径不存在会创建这个路径
         print("---  OK  ---")
     else:
-        print("---  log folder: ", path, "exists!  ---", sep="")
+        print("---  log folder: ", path, " exists!  ---", sep="")
         
 def mkfile(file):  #自定义创建文件函数
     is_log_file = os.path.exists(file)
@@ -20,5 +20,5 @@ def mkfile(file):  #自定义创建文件函数
         print("---  OK  ---")
         logfile.close()
     else:
-        print("---  log file: ", file, "exists!  ---", sep="")
+        print("---  log file: ", file, " exists!  ---", sep="")
         
